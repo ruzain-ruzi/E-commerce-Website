aabf22914de948e092e40cb31b83548c0c98f2eb
ruzain <ruzainruzi90@gmail.com>
2022-12-14 23:18:21 +0530

    database changed

--- Emart_Online_Shopping/settings.py
+++ Emart_Online_Shopping/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'Emart_Online_Shopping.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     # 'NAME': BASE_DIR / 'db.sqlite3',
-    #     'NAME': 'onlineshopping',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'PORT': '3306'
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': "db.htctvkziatwakbkjegwp.supabase.co",
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'DQouuBOQMPENsdeg',
+        'PORT': '5432'
+    }
 }
 
 # Password validation
